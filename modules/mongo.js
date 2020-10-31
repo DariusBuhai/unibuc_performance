defe6f9dbbec9b8f6ae10f6659eaaba23eb92a31
tifuiWork <itifui00@gmail.com>
2020-10-31 20:39:44 +0200

    maybe works

--- modules/mongo.js
+++ modules/mongo.js
@@ -1,55 +1,60 @@
 const url = "mongodb + srv://unibuc_performance:smarthack_2020@cluster0.lsba1.mongodb.net/Cluster0?retryWrites=true&w=majority";
 const MongoClient = require('mongodb').MongoClient;
 
-function insertBuilding(building) {
+class DBManagement {
 
-    // fot testing only
-    // building = {
-    //     id: 1, name: 'Steve', address: 'Jobs', hospotsNo: 10, maxCap: 100,
-    //     peopleByHours: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
-    // };
+    insertBuilding(building) {
 
-    MongoClient.connect(url, function (err, db) {
-        if (err) throw err;
+        // fot testing only
+        // building = {
+        //     id: 1, name: 'Steve', address: 'Jobs', hospotsNo: 10, maxCap: 100,
+        //     peopleByHours: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
+        // };
 
-        db.collection('Buildings', function (err, collection) {
-            collection.insert(building);
-        });
-    });
-}
+        MongoClient.connect(url, function (err, db) {
+            if (err) throw err;
 
-function getBuilding(buildingId) {
-    let retVal = null;
-
-    MongoClient.connect(url, function (err, db) {
-        if (err) throw err;
-
-        db.collection('Buildings', function (err, collection) {
-            collection.find().toArray(function (err, items) {
-                if (err) throw err;
-                
-                for (let i = 0; i < items.length(); ++i)
-                    if (items[i].id == buildingId) {
-                        retVal = items[i];
-                        break;
-                    }
+            db.collection('Buildings', function (err, collection) {
+                collection.insert(building);
             });
         });
-    });
-    
-    return retVal;
-}
+    }
 
-function updateBuilding(newBuilding) {
-    MongoClient.connect("mongodb://localhost:27017/MyDb", function (err, db) {
-        if (err) throw err;
+    getBuilding(buildingId) {
+        let retVal = null;
 
-        db.collection('Buildings', function (err, collection) {
-            collection.updateOne({ id: newBuilding.id }, newBuilding, function(err, res) {
-                if (err) throw err;
+        MongoClient.connect(url, function (err, db) {
+            if (err) throw err;
+
+            db.collection('Buildings', function (err, collection) {
+                collection.find().toArray(function (err, items) {
+                    if (err) throw err;
+                    
+                    for (let i = 0; i < items.length(); ++i)
+                        if (items[i].id == buildingId) {
+                            retVal = items[i];
+                            break;
+                        }
+                });
             });
         });
-    });
+        
+        return retVal;
+    }
+
+    updateBuilding(newBuilding) {
+        MongoClient.connect("mongodb://localhost:27017/MyDb", function (err, db) {
+            if (err) throw err;
+
+            db.collection('Buildings', function (err, collection) {
+                collection.updateOne({ id: newBuilding.id }, newBuilding, function(err, res) {
+                    if (err) throw err;
+                });
+            });
+        });
+    }
 }
+
+export default DBManagement;
 
 // to be continued
