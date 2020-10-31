33e54807c371a13b8d79b63d2c15ab5c05765855
Buhai Darius <buhaidarius@gmail.com>
2020-10-31 19:47:37 +0200

    Update

--- static/model.js
+++ static/model.js
@@ -1,33 +1,56 @@
-// load model view
 
-var myViewerDiv = document.getElementById('MyViewerDiv');
-var viewer = new Autodesk.Viewing.Private.GuiViewer3D(myViewerDiv);
-var options = {
-    'env': 'Local',
-    'document': 'models/210 King/Resource/3D_View/_3D_ 1583181/_3D_.svf',
-};
-Autodesk.Viewing.Initializer(options, function () {
-    viewer.start(options.document, options);
-});
-
-// display informations about the building
-
-// past info about the building
-let date = new Date();
-let currHour = date.getHours();
-let pastContainer = document.getElementById('past');
-
-if (currHour == 0) {
-    let element = document.createElement('p');
-    element.innerText = 'No information avaliable for today';
-
-    pastContainer.appendChild(element);
+function load_model(){
+    var myViewerDiv = document.getElementById('MyViewerDiv');
+    var viewer = new Autodesk.Viewing.Private.GuiViewer3D(myViewerDiv);
+    var options = {
+        'env': 'Local',
+        'document': 'models/210 King/Resource/3D_View/_3D_ 1583181/_3D_.svf',
+    };
+    Autodesk.Viewing.Initializer(options, function () {
+        viewer.start(options.document, options);
+    });
 }
 
-for (let i = 0; i < currHour; ++i) {
-    let element = document.createElement('p');
-    element.innerText = 'See info from ' + toString(i) + ':00';
+function generate_hour_chart(){
+    let date = new Date();
+    let currHour = date.getHours();
+    let pastContainer = document.getElementById('past');
+    let hours = {};
 
-    pastContainer.appendChild(element);
+    for (let i = 0; i < currHour; ++i){
+        hours[i] = Math.floor(Math.random() * 100);
+    }
+    console.log(hours);
+
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: Object.keys(hours),
+            datasets: [{
+                label: '# of Votes',
+                data: Object.values(hours),
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
 }
+
+load_model();
+generate_hour_chart();
 
