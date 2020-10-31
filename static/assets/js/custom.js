2f7dfd15214eda9f28b6805bc08d2e213684640c
Buhai Darius <buhaidarius@gmail.com>
2020-10-31 16:06:38 +0200

    Update

---
+++ static/assets/js/custom.js
@@ -0,0 +1,26 @@
+
+function toggleHideByClass(className, hide = true){
+    let el = document.getElementsByClassName(className);
+    for(let i=0;i<el.length;i++)
+        el[i].hidden= hide;
+}
+
+function toggleRemoveByClass(className, classToRemove){
+    let el = document.getElementsByClassName(className);
+    for(let i=0;i<el.length;i++)
+        el[i].classList.remove(classToRemove);
+}
+
+function changeActiveSection(id){
+    toggleRemoveByClass("section-item","active");
+    document.getElementsByClassName("section-item-"+id)[0].classList.add("active");
+    toggleHideByClass("project-section-item", true);
+    document.getElementsByClassName("project-section-item-"+id)[0].hidden = false;
+}
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-163958714-1');
