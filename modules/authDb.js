216af2d009900c61e5fb38cdc312350c70ca5363
tifuiWork <itifui00@gmail.com>
2020-10-31 23:23:08 +0200

    all users are admins

--- modules/authDb.js
+++ modules/authDb.js
@@ -1,7 +1,7 @@
 const url = "mongodb + srv://unibuc_performance:smarthack_2020@cluster0.lsba1.mongodb.net/Cluster0?retryWrites=true&w=majority";
 const MongoClient = require('mongodb').MongoClient;
 
-// Admins:
+// Users(Admins):
 // id
 // username
 // password
