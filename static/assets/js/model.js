335f8873142e4be4a0bbf585b304738bdcb3db9b
Stefan Radu <stefan270101@gmail.com>
2020-10-31 23:12:45 +0200

    Click to add sphere wherever

--- static/assets/js/model.js
+++ static/assets/js/model.js
@@ -5,43 +5,56 @@ var options = {
     'document': 'models/House/f0224dd3-8767-45c1-ff99-5c9c881b9fee/0.svf',
 };
 
+let spheres = [];
+
 function load_model(){
     Autodesk.Viewing.Initializer(options, function () {
         viewer.start(options.document, options);
     });
 }
 
-function addSphere() {        
+function addSphere(coord) {        
   //create material red
   var material_red = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 
-  //get bounding box of the model
+  // //get bounding box of the model
+  //
+  // var boundingBox = viewer.model.getBoundingBox();
+  //
+  // var maxpt = boundingBox.max;
+  // var minpt = boundingBox.min;
+  //
+  // var xdiff = maxpt.x - minpt.x;
+  // var ydiff = maxpt.y - minpt.y;
+  // var zdiff = maxpt.z - minpt.z;
+  //
+  // //set a nice radius in the model size
+  //
+  // var niceRadius = Math.pow((xdiff * xdiff + ydiff * ydiff + zdiff * zdiff), 0.5) / 10;
+  //
+  // //createsphere1 and place it at max point of boundingBox
 
-  var boundingBox = viewer.model.getBoundingBox();
-
-  var maxpt = boundingBox.max;
-  var minpt = boundingBox.min;
-
-  var xdiff = maxpt.x - minpt.x;
-  var ydiff = maxpt.y - minpt.y;
-  var zdiff = maxpt.z - minpt.z;
-
-  //set a nice radius in the model size
-
-  var niceRadius = Math.pow((xdiff * xdiff + ydiff * ydiff + zdiff * zdiff), 0.5) / 10;
-
-  //createsphere1 and place it at max point of boundingBox
-
-  var geom = new THREE.SphereGeometry(niceRadius, 20);
+  var geom = new THREE.SphereGeometry(20, 20);
   var sphereMesh = new THREE.Mesh(geom, material_red);
 
-  sphereMesh.position.set(maxpt.x, maxpt.y, maxpt.z);
+  sphereMesh.position.set(coord.x, coord.y, coord.z);
+  spheres.push(sphereMesh);
 
   if (!viewer.overlays.hasScene('scene1')) {
     viewer.overlays.addScene('scene1');
   }
   viewer.overlays.addMesh(sphereMesh, 'scene1');
 }
+
+var onKeyDown = function(event) {
+  if (event.keyCode == 67) { // when 'c' is pressed
+    for (sph of spheres) {
+      sph.material.color.setHex(0x00ff00); // there is also setHSV and setRGB
+    }
+    viewer.refresh();
+  }
+};
+document.addEventListener('keydown', onKeyDown, false);
 
 function generate_hour_chart(){
     let date = new Date();
@@ -83,13 +96,39 @@ function generate_hour_chart(){
     });
 }
 
-console.log(1);
+function getClickCoordinates(event) {
+  let intersections = [];
+  const bounds = document.getElementById('MyViewerDiv')
+    .getElementsByTagName('canvas')[0].getBoundingClientRect();
+  viewer.impl.castRayViewport(viewer.impl.clientToViewport(event.clientX - bounds.left, event.clientY - bounds.top), false, null, null, intersections);
+  if (intersections.length > 0) {
+    const intersection = intersections[0];
+
+    return {
+      x: intersection.point.x.toFixed(2),
+      y: intersection.point.y.toFixed(2),
+      z: intersection.point.z.toFixed(2),
+    };
+  }
+}
+
+
+function addSphereOnClick(event) {
+  coord = getClickCoordinates(event);
+  console.log(coord);
+  if (coord) {
+    addSphere(coord);
+  }
+}
+
+
 viewer.addEventListener(Autodesk.Viewing.SELECTION_CHANGED_EVENT, (event) => {
   addSphere();
   console.log('ceva');
   console.log(event);
 });
-console.log(2);
 
 load_model();
 generate_hour_chart();
+
+document.getElementById('MyViewerDiv').addEventListener('click', addSphereOnClick);
