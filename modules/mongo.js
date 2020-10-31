7ad2ebe4bd5d30913900dda255bd48ca5b63dc03
tifuiWork <itifui00@gmail.com>
2020-10-31 17:06:09 +0200

    connection to mongoDB

--- modules/mongo.js
+++ modules/mongo.js
@@ -1,2 +1,23 @@
-var mongo = require('mongodb');
+const mongo = require('mongodb');
 
+async function main() {
+    const url = "mongodb + srv://unibuc_performance:smarthack_2020@cluster0.lsba1.mongodb.net/Cluster0?retryWrites=true&w=majority";
+    const client  = new MongoClient(url);
+
+
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+        await listDatabases(client);
+    }
+    catch (e) {
+        console.error(e);
+    }
+    finally {
+        await client.close();
+    }
+}
+
+main().catch(console.error);
+
+// to be continued
