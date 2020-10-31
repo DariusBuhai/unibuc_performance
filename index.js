16028ce85f8f72bdf9fb317653e5153179374b91
Buhai Darius <buhaidarius@gmail.com>
2020-10-31 14:16:39 +0200

    Update

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 var ForgeSDK = require('forge-apis');
 var FORGE_CLIENT_ID = 'UGtHbq5PBjPXdicgpOWQtM1hHgL4yaDU' , FORGE_CLIENT_SECRET = 'ftck1wYAwbsFFSr7';
 
-// Initialize the 2-legged OAuth2 client, set specific scopes and optionally set the `autoRefresh` parameter to true
-// if you want the token to auto refresh
 var autoRefresh = true; // or false
 
-var oAuth2TwoLegged = new ForgeSDK.AuthClientTwoLegged(FORGE_CLIENT_ID, FORGE_CLIENT_SECRET, [
-    'data:read',
-    'data:write'
-], autoRefresh);
+async function get_credentials(){
+    var oAuth2TwoLegged = new ForgeSDK.AuthClientTwoLegged(FORGE_CLIENT_ID, FORGE_CLIENT_SECRET, [
+        'data:read',
+        'data:write'
+    ], autoRefresh);
 
-oAuth2TwoLegged.authenticate().then(function(credentials){
-    console.log(credentials);
-    // The `credentials` object contains an access_token that is being used to call the endpoints.
-    // In addition, this object is applied globally on the oAuth2TwoLegged client that you should use when calling secure endpoints.
-}, function(err){
-    console.error(err);
-});
+    return await oAuth2TwoLegged.authenticate();
+}
 
-// Stefan a fost aici
+const http = require('http');
+
+const requestListener = async function (req, res) {
+    let credentials = await get_credentials();
+    res.writeHead(200);
+    res.end(credentials.access_token);
+}
+
+const server = http.createServer(requestListener);
+server.listen(8080);
