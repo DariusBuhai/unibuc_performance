8c9b65ee29a27d02ecadc0f657de98a0bc6cc655
tifuiWork <itifui00@gmail.com>
2020-10-31 21:40:53 +0200

    finished prediction

--- modules/prediction.js
+++ modules/prediction.js
@@ -2,5 +2,14 @@ const ip = require('interpolating-polynomial');
 const db = require('mongo.js');
 
 function makePrediction(hourValue) {
-    
+    let dataset = db.getHoursList();
+    let points = [];
+
+    for(let i = 0; i < dataset.length(); ++i) {
+        points.push([i, dataset[i].date.getHours()]);
+    }
+
+    f = ip(points);
+
+    return f(hourValue);
 }
